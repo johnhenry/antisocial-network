702be8f7bedf4939be0da5101057bc9429efc8a5
John <john@iamjohnhenry.com>
2024-07-30 23:01:04 -0700

    save handlers.ts

--- src/tools/handlers.ts
+++ src/tools/handlers.ts
@@ -1,24 +1,22 @@
-import type { Tool , Handler} from "@/types/tools";
+import type { Handler, Tool } from "@/types/tools";
 import subtraction from "./subtraction/handler";
-import assessagents from "./assess-agents/handler";
 import timetool from "./timetool/handler";
 import javascript from "./javascript/handler";
 import openmeteoweather from "./openmeteoweather/handler";
 import descriptorRegistry from "@/tools/descriptors";
 
-const handlers = { assessagents, subtraction, timetool, javascript, openmeteoweather }
+const handlers = { subtraction, timetool, javascript, openmeteoweather };
 const registry: Record<string, Tool> = {};
 const register = (
   handler: Handler,
-  name: string = handler.name||"",) => {
-  if(descriptorRegistry[name]){
+  name: string = handler.name || "",
+) => {
+  if (descriptorRegistry[name]) {
     registry[name] = { ...descriptorRegistry[name], handler };
   }
 };
-for(const [name, handler] of Object.entries(handlers)){
+for (const [name, handler] of Object.entries(handlers)) {
   register(handler, name);
 }
 
 export default registry;
-
-
